800c740474473243aac26d2b2f50b20bd27dfcd8
olc-k <aleksandrapalasz01@gmail.com>
2026-08-11 17:40:12 +0200

    Define raw layoffs table and import data

--- layoffs_cleaning.sql
+++ layoffs_cleaning.sql
@@ -1,2 +1,19 @@
 CREATE DATABASE world_layoffs;
 USE world_layoffs;
+
+/*
+Note: The date column is imported as text because its format in the raw file does not meet SQL standards. 
+This will be fixed during the standardization stage.
+*/
+
+CREATE TABLE layoffs (
+    company TEXT,
+    location TEXT,
+    industry TEXT,
+    total_laid_off TEXT,
+    percentage_laid_off TEXT,
+    date TEXT,
+    stage TEXT,
+    country TEXT,
+    funds_raised_millions TEXT
+);
