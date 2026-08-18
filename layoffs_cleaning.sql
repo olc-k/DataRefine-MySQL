25c947b9bce862678d286fc579e27d29dd5008b5
olc-k <aleksandrapalasz01@gmail.com>
2026-08-19 00:00:45 +0200

    Populate staging2 with row_num

--- layoffs_cleaning.sql
+++ layoffs_cleaning.sql
@@ -61,3 +61,20 @@ CREATE TABLE `world_layoffs`.`layoffs_staging2` (
   `row_num` INT
 );
 
+-- Insert data while partitioning and numbering duplicate rows
+INSERT INTO `world_layoffs`.`layoffs_staging2`
+SELECT 
+    `company`,
+    `location`,
+    `industry`,
+    `total_laid_off`,
+    `percentage_laid_off`,
+    `date`,
+    `stage`,
+    `country`,
+    `funds_raised_millions`,
+    ROW_NUMBER() OVER (
+        PARTITION BY company, location, industry, total_laid_off, 
+                     percentage_laid_off, `date`, stage, country, funds_raised_millions
+    ) AS row_num
+FROM world_layoffs.layoffs_staging;
